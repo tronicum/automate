23bef476521db7100345d553083b22715b68bb00
stefan-sels-flaconi <stefan.sels@flaconi.de>
2016-04-20 19:12:43 +0200

    apt-key

--- saltstack/saltstack.sh
+++ saltstack/saltstack.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #do not use in production!
-echo "deb http://repo.saltstack.com/apt/debian/8/amd64/latest jessie main" >> /etc/apt/sources.list.d/saltstack.list
+echo "deb http://repo.saltstack.com/apt/debian/8/amd64/latest jessie main" > /etc/apt/sources.list.d/saltstack.list
+wget -O - https://repo.saltstack.com/apt/debian/8/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
 apt-get update
 apt-get install salt-master
 apt-get install salt-minion
