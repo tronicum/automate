fcd64d76e3a428f404a72c5999fa9e6a8c849da8
stefan-sels-flaconi <stefan.sels@flaconi.de>
2016-04-20 19:04:42 +0200

    saltstack initial

---
+++ saltstack/saltstack.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#do not use in production!
+echo „deb http://repo.saltstack.com/apt/debian/8/amd64/latest jessie main“ >> /etc/apt/sources.list.d/saltstack.list
+apt-get update
+apt-get install salt-master
+apt-get install salt-minion
+apt-get install salt-ssh
+apt-get install salt-syndic
+apt-get install salt-cloud
+apt-get install salt-api
