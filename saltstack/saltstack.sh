6dcad3d525855fa750afe5a2b4bb2577672828c6
stefan-sels-flaconi <stefan.sels@flaconi.de>
2016-04-20 19:07:49 +0200

    shitt " double quotes

--- saltstack/saltstack.sh
+++ saltstack/saltstack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #do not use in production!
-echo „deb http://repo.saltstack.com/apt/debian/8/amd64/latest jessie main“ >> /etc/apt/sources.list.d/saltstack.list
+echo "deb http://repo.saltstack.com/apt/debian/8/amd64/latest jessie main" >> /etc/apt/sources.list.d/saltstack.list
 apt-get update
 apt-get install salt-master
 apt-get install salt-minion
